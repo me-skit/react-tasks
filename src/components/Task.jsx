daa4fbbb05acd6926a2f01045efa74e7c54de422
Meme <meme_es@hotmail.com>
2022-06-09 17:23:35 -0600

    Improve Task component
    This commit adds extra props for an id, a function to set a task as
    completed and another one to delete the task.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import '../stylesheets/Task.css';
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Task({ text, isCompleted }) {
+function Task({ id, text, isCompleted, completeTask, deleteTask }) {
   return (
     <div className={isCompleted ? 'task-container completed' : 'task-container'}>
-      <div className='task-text'>
+      <div
+        className='task-text'
+        onClick={() => completeTask(id)}>
         {text}
       </div>
-      <div className='task-icons-container'>
+      <div
+        className='task-icons-container'
+        onClick={() => deleteTask(id)}>
         <AiOutlineCloseCircle className='task-icon' />
       </div>
     </div>

35a930e6a34e49b9c0ffcf0882b40482756d670d
Meme <meme_es@hotmail.com>
2022-06-09 18:57:59 -0600

    Add the "Add Task" functionality

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../stylesheets/TaskForm.css";
+import { v4 as uuidv4 } from 'uuid';
 
 function TaskForm(props) {
+
+  const [input, setInput] =useState('');
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+
+  const submitTask = event => {
+    event.preventDefault();
+
+    const newTask = {
+      id: uuidv4(),
+      text: input,
+      completed: false
+    }
+
+    props.onSubmit(newTask);
+  }
+
   return (
-    <form className='task-form'>
+    <form
+      className='task-form'
+      onSubmit={submitTask}>
       <input
         className='task-input'
         type='text'
         placeholder='Write a task'
         name='text'
+        onChange={handleChange}
       />
       <button className='task-button'>Add Task</button>
     </form>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,14 +7,24 @@ function TaskList() {
 
   const [tasks, setTasks] = useState([]);
 
+  const addTask = task => {
+    console.log(task);
+    task.text = task.text.trim();
+    if (task.text) {
+      const updatedTasks = [task, ...tasks]
+      setTasks(updatedTasks);
+    }
+  };
   
   return (
     <>
-      <TaskForm />
+      <TaskForm onSubmit={addTask} />
       <div className='task-list-container'>
         {
           tasks.map(task => 
             <Task
+              key={task.id}
+              id={task.id}
               text={task.text} 
               isCompleted={task.completed}
             />
